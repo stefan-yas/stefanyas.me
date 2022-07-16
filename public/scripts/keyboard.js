25b1e31b50bb8d1fc47a146732132ac81f7d23fd
Stefan Yas <stefan.sy.yas@gmail.com>
2022-07-16 18:35:04 +0200

    completely new design

---
+++ public/scripts/keyboard.js
@@ -0,0 +1,59 @@
+function playC() {
+    var audio = document.getElementById("audioC");
+    audio.play();
+}
+
+function playCSharp() {
+    var audio = document.getElementById("audioCSharp");
+    audio.play();
+}
+
+function playD() {
+    var audio = document.getElementById("audioD");
+    audio.play();
+}
+
+function playDSharp() {
+    var audio = document.getElementById("audioDSharp");
+    audio.play();
+}
+
+function playE() {
+    var audio = document.getElementById("audioE");
+    audio.play();
+}
+
+function playF() {
+    var audio = document.getElementById("audioF");
+    audio.play();
+}
+
+function playFSharp() {
+    var audio = document.getElementById("audioFSharp");
+    audio.play();
+}
+
+function playG() {
+    var audio = document.getElementById("audioG");
+    audio.play();
+}
+
+function playGSharp() {
+    var audio = document.getElementById("audioGSharp");
+    audio.play();
+}
+
+function playA() {
+    var audio = document.getElementById("audioA");
+    audio.play();
+}
+
+function playASharp() {
+    var audio = document.getElementById("audioASharp");
+    audio.play();
+}
+
+function playB() {
+    var audio = document.getElementById("audioB");
+    audio.play();
+}
